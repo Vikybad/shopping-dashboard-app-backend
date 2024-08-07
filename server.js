aff208a20c0849715a12edae47e31bf83316d715
vikramBadesara <vikram.badesara@fretron.com>
2024-08-07 20:14:29 +0530

    ad

--- server.js
+++ server.js
@@ -17,6 +17,10 @@ app.use('/api/users', require('./routes/users'));
 app.use('/api/orders', require('./routes/orders'));
 app.use('/api/tasks', require('./routes/tasks'));
 
+app.get('/', (req, res) => {
+    res.send({ Error: "invalid path" })
+})
+
 const PORT = process.env.BACKEND_PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
